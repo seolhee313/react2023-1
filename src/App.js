9ecbf6e2343ae02796cbc55ca75d61c5c73075f5
seolhee313 <125417882+seolhee313@users.noreply.github.com>
2023-05-08 13:00:17 +0900

    2023.05.08

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// function clock(){
+//   let clock = document.getElementById("clock");
+
+//   setInterval(function(){
+//     clock.innerHTML = new Date().toLocaleDateString();
+//   }, 1000);
+// }
+// clock();
+
+function App(){
+  return (
+    <div>
+      <h1>hello 리액트</h1>
+    </div>
+  );
+}
+
+export default App;
